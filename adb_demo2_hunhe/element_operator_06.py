9f598213e50a3772819604e125474da3348e9126
limusen <m1557493385@163.com>
2021-09-13 17:16:03 +0800

    tijiao

--- adb_demo2_hunhe/element_operator_06.py
+++ adb_demo2_hunhe/element_operator_06.py
@@ -6,7 +6,6 @@
 
 import time
 from appium import webdriver
-from appium.webdriver.connectiontype import ConnectionType
 
 des = {
     "platformName": "Android",
@@ -24,15 +23,15 @@ des = {
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', des)
 
 
-#
-# driver.find_element_by_xpath('//android.widget.ImageView[@bounds="[0,1170][360,1338]"]').click()
-# time.sleep(1)
-# driver.find_element_by_xpath('//android.widget.ImageView[@bounds="[360,1338][720,1506]"]').click()
-# time.sleep(1)
-#
-# # 　新建联系人然后清空输入框
-# driver.find_element_by_xpath('//android.widget.RelativeLayout[@bounds="[0,72][1080,216]"]').click()
-# driver.find_element_by_xpath('//android.widget.EditText[@text="姓名"]')
 
-# 调整音量
-driver.long_press_keycode(24)
+driver.find_element_by_xpath('//android.widget.ImageView[@bounds="[0,1170][360,1338]"]').click()
+time.sleep(1)
+driver.find_element_by_xpath('//android.widget.ImageView[@bounds="[360,1338][720,1506]"]').click()
+time.sleep(1)
+
+# 　新建联系人然后清空输入框
+driver.find_element_by_xpath('//android.widget.RelativeLayout[@bounds="[0,72][1080,216]"]').click()
+driver.find_element_by_xpath('//android.widget.EditText[@text="姓名"]')
+
+# # 调整音量
+# driver.long_press_keycode(24)
