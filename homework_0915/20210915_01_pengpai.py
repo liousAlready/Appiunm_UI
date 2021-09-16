847bd39ab14c357666a3f0505a8dcc52d66102dd
limusen <m1557493385@163.com>
2021-09-16 17:45:52 +0800

    9/15 作业开头提交

---
+++ homework_0915/20210915_01_pengpai.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/14 8:01 下午
+# @Author  : Li
+# @Email   : m15574933885@163.com
+# @File    : 20210912_01_setting.py
+# @Software: PyCharm
+# @desc :
+
+
+import time
+from appium import webdriver
+from appium.webdriver.webdriver import By
+
+# des = {
+#     "platformName": "Android",
+#     "platformVersion": "9",
+#     "deviceName": "mac虚拟机",
+#     "appPackage": "com.wondertek.paper",
+#     "appActivity": "cn.thepaper.paper.ui.main.MainActivity",
+#     "udid": "192.168.56.104:5555",
+#     "noReset": "True"
+# }
+
+des = {
+    "platformName": "Android",
+    "platformVersion": "8.1.0",
+    "deviceName": "vivo x20",
+    # "appPackage": "com.wondertek.paper",
+    # "appActivity": "cn.thepaper.paper.ui.main.MainActivity",
+    "appPackage": "com.wondertek.paper",
+    "appActivity": "cn.thepaper.paper.ui.splash.welcome.LaunchActivity",
+    "udid": "c6c8c4ce",
+    "noReset": True,
+    "newCommandTimeout": 30  # 30s没对手机发送新命令，就断开连接
+    # 　 "com.wondertek.paper/cn.thepaper.paper.ui.splash.welcome.LaunchActivity"
+}
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', des)
+driver.implicitly_wait(10)
+time.sleep(2)
+# 查看温度
+w = driver.find_element(By.XPATH,
+                        '//android.widget.LinearLayout[@resource-id="com.wondertek.paper:id/ths_weather_layout" and @index="1"]').text
+print(w)
+
+# 查看朋友圈是否能够点击
+display = driver.find_element(By.XPATH, '//android.widget.TextView[@text="澎友圈"]').is_enabled()
+print(display)
