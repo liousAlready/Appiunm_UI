320f4d1bc712d4521cef42d94c6ed40c79d5fe04
limusen <m1557493385@163.com>
2021-09-17 13:54:19 +0800

    作业修改获取方式

--- homework_0915/20210915_01_pengpai.py
+++ homework_0915/20210915_01_pengpai.py
@@ -11,29 +11,29 @@ import time
 from appium import webdriver
 from appium.webdriver.webdriver import By
 
-des = {
-    "platformName": "Android",
-    "platformVersion": "9",
-    "deviceName": "mac虚拟机",
-    "appPackage": "com.wondertek.paper",
-    "appActivity": "cn.thepaper.paper.ui.main.MainActivity",
-    "udid": "192.168.56.104:5555",
-    "noReset": "True"
-}
-
 # des = {
 #     "platformName": "Android",
-#     "platformVersion": "8.1.0",
-#     "deviceName": "vivo x20",
-#     # "appPackage": "com.wondertek.paper",
-#     # "appActivity": "cn.thepaper.paper.ui.main.MainActivity",
+#     "platformVersion": "9",
+#     "deviceName": "mac虚拟机",
 #     "appPackage": "com.wondertek.paper",
-#     "appActivity": "cn.thepaper.paper.ui.splash.welcome.LaunchActivity",
-#     "udid": "c6c8c4ce",
-#     "noReset": True,
-#     "newCommandTimeout": 30  # 30s没对手机发送新命令，就断开连接
-#     # 　 "com.wondertek.paper/cn.thepaper.paper.ui.splash.welcome.LaunchActivity"
+#     "appActivity": "cn.thepaper.paper.ui.main.MainActivity",
+#     "udid": "192.168.56.104:5555",
+#     "noReset": "True"
 # }
+
+des = {
+    "platformName": "Android",
+    "platformVersion": "8.1.0",
+    "deviceName": "vivo x20",
+    # "appPackage": "com.wondertek.paper",
+    # "appActivity": "cn.thepaper.paper.ui.main.MainActivity",
+    "appPackage": "com.wondertek.paper",
+    "appActivity": "cn.thepaper.paper.ui.splash.welcome.LaunchActivity",
+    "udid": "c6c8c4ce",
+    "noReset": True,
+    "newCommandTimeout": 30  # 30s没对手机发送新命令，就断开连接
+    # 　 "com.wondertek.paper/cn.thepaper.paper.ui.splash.welcome.LaunchActivity"
+}
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', des)
 driver.implicitly_wait(10)
@@ -44,8 +44,10 @@ w = driver.find_element(By.XPATH,
                         '//android.widget.TextView[@resource-id="com.wondertek.paper:id/ths_weather_tv"]').text
 print(w)
 
+time.sleep(2)
 # 查看朋友圈是否能够点击
-enabled = driver.find_element(By.XPATH,
-                              '//android.widget.ImageView[@bounds="[940,2628][1052,2730]"]').is_enabled()
-# display = driver.find_element(By.XPATH, '//android.widget.TextView[@text="澎友圈"]').is_enabled()
+# enabled = driver.find_element(By.XPATH, '//android.widget.ImageView[@bounds="[940,2628][1052,2730]"]').is_enabled()
+# print(enabled)
+
+enabled = driver.find_element(By.XPATH, '//android.widget.TextView[@text="澎友圈"]').is_enabled()
 print(enabled)
